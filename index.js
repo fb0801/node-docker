3805412a305a7952367a9bd870b39a70025f660a
fb0801 <fbhatti0801@gmail.com>
2024-09-04 17:57:05 +0100

    config

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express")
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config");
 
 
 const app = express();
 
 
-mongoose.connect("mongodb://nahraf:FOSSIL@mongo:27017/?authSource=admin")
+mongoose
+.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`)
 .then(() => console.log("successfully connected to DB"))
-    .catch((e) =>console.log(e));
-
-
+.catch((e) =>console.log(e));
 
 
 app.get("/", (req, res) => {
