25a44593aa04bfe7727311ac5595f66658828e3c
fb0801 <fbhatti0801@gmail.com>
2024-09-06 18:15:04 +0100

    post controller 3 create posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -40,3 +40,23 @@ exports.getOnePost = async (req, res, next) => {
         })
     }
 }
+
+
+exports.createPost = async (req, res, next) => {
+    try{
+        const post = await Post.create(req.body)
+
+        res.status(200).json({
+            status: "success",
+
+            data: {
+                post,
+
+            },
+        })
+    } catch (e){
+        res.status(400).json({
+            status:"fail",
+        })
+    }
+}
