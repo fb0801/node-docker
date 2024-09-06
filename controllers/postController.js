9ead4bf61a338f2e00368cd9bd487a4a56647121
fb0801 <fbhatti0801@gmail.com>
2024-09-06 18:13:17 +0100

    post controller 2 get posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -15,7 +15,28 @@ exports.getAllPosts = async(req, res, next) => {
 
 
     } catch (e) {
-        res.status(400)
+        res.status(400).json({
+            status: "fail"
+        })
     }
     
+}
+
+exports.getOnePost = async (req, res, next) => {
+    try{
+        const post = await Post.findById(req.params.id)
+
+        res.status(200).json({
+            status: "success",
+
+            data: {
+                post,
+
+            },
+        })
+    } catch (e){
+        res.status(400).json({
+            status:"fail",
+        })
+    }
 }
