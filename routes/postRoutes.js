231a24de2d490cc13718986e40d5a78fb6bb9878
fb0801 <fbhatti0801@gmail.com>
2024-09-11 14:14:24 +0100

    post controller routes 2

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,3 +2,14 @@ const express = require("express")
 
 const postController = require("../controllers/postController")
 
+const router = express.Router()
+
+router.route("/").get(postController.getAllPosts).post(postController.createPost)
+
+router.route("/:id")
+.get(postController.getOnePost)
+.patch(postController.updatePost)
+.delete(postController.deletePost);
+
+
+module.exports = router;
