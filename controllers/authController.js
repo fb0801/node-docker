960db4cd7554d78af35f6f8e1c5bbdc6fc69c2d9
fb0801 <fbhatti0801@gmail.com>
2024-09-11 15:18:23 +0100

    users auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,20 +1,47 @@
 const User = require("../models/userModel")
 
+const bcrypt = require("bcryptjs")
+
 exports.signUp = async (req, res) => {
     const {username, password} = req.body
 
-
     try{
-        const newUser = await User.create(req.body)
+        const hashpassword = await bcrypt.hash(password,12)
+        const newUser = await User.create({
+            username,
+            password: hashpassword
+        })
         res.status(201).json({
             status:"success",
             data: {
-                user: newUser
-            }
+                user: newUser,
+            },
         })
     } catch (e) {
         res.status(400).json({
             status: "fail",
         })
     }
+}
+
+exports.login = async (req, res) => {
+    const {username,password} = req.body;
+try{
+        const hashpassword = await bcrypt.hash(password,12)
+        const newUser = await User.create({
+            username,
+            password: hashpassword
+        })
+        res.status(201).json({
+            status:"success",
+            data: {
+                user: newUser,
+            },
+        })
+    } catch (e) {
+        res.status(400).json({
+            status: "fail",
+        })
+    }
+
 }
