5bb40626462edb538e762723001b83a2f7afb8cb
fb0801 <fbhatti0801@gmail.com>
2024-09-11 15:10:41 +0100

    users 3

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,9 @@
-const User = require("../models/userModels")
+const User = require("../models/userModel")
 
 exports.signUp = async (req, res) => {
+    const {username, password} = req.body
+
+
     try{
         const newUser = await User.create(req.body)
         res.status(201).json({
