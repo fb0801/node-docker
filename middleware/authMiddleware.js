181932a8b9ffbbb5a88639f390b77847b61ef9f2
fb0801 <fbhatti0801@gmail.com>
2024-09-23 15:47:55 +0100

    middleware 2

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,4 +4,9 @@ const protect = (req, res, next) => {
     if (!user) {
         return res.status(401).json({status: 'fail', message:'unauthorized'})
     }
+    req.user = user
+
+    next()
 }
+
+module.exports = protect
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,9 +2,13 @@ const express = require("express")
 
 const postController = require("../controllers/postController")
 
+const protect = require("../middleware/authMiddleware")
+
 const router = express.Router()
 
-router.route("/").get(postController.getAllPosts).post(postController.createPost)
+router.route("/")
+.get(postController.getAllPosts)
+.post(postController.createPost)
 
 router.route("/:id")
 .get(postController.getOnePost)
